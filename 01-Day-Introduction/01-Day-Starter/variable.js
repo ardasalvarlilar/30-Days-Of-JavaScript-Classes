398876c5d8c12134df2ef2cf00f0ae79e5053ca9
Arda Şalvarlılar <93258286+ardasalvarlilar@users.noreply.github.com>
2023-06-14 12:25:36 +0300

    Uploaded Day 1 Class

---
+++ 01-Day-Introduction/01-Day-Starter/variable.js
@@ -0,0 +1,95 @@
+// DATA TYPES
+
+// NUMBERS
+
+/*
+Integers: Integer (negative, zero and positive) numbers Example: ... -3, -2, -1, 0, 1, 2, 3 ...
+Float-point numbers: Decimal number Example ... -3.5, -2.25, -1.0, 0.0, 1.1, 2.2, 3.5 ...
+*/
+
+// STRINGS
+/*
+'a'
+'Asabeneh'
+"Asabeneh"
+'Finland'
+'JavaScript is a beautiful programming language'
+'I love teaching'
+'I hope you are enjoying the first day'
+`We can also create a string using a backtick`
+'A string could be just as small as one character or as big as many pages'
+'Any data type under a single quote, double quote or backtick is a string'
+*/
+
+// Booleans
+/*
+true // if the light is on, the value is true
+false // if the light is off, the value is false
+*/
+
+// Undefined
+/* 
+let firstName
+console.log(firstName) // undefined, because it is not assigned to a value yet
+*/
+
+// Null
+/*
+let emptyValue = null
+*/
+
+// Checking Data Types
+console.log(typeof 'Asabeneh') // string
+console.log(typeof 5) // number
+console.log(typeof true) // boolean
+console.log(typeof null) // object type
+console.log(typeof undefined) // undefined
+
+// javaScript valid variables
+/*
+  firstName
+  lastName
+  country
+  city
+  capitalCity
+  age
+  isMarried
+
+  first_name
+  last_name
+  is_married
+  capital_city
+
+  num1
+  num_1
+  _num_1
+  $num1
+  year2020
+  year_2020
+*/
+
+// javaScript invalid variables
+/*
+  first-name
+  1_num
+  num_#_1
+*/
+
+// declared variables
+// Declaring different variables of different data types
+let firstName = 'Arda' // first name of a person
+let lastName = 'Şalvarlılar' // last name of a person
+let country = 'Türkiye' // country
+let city = 'Izmir' // capital city
+let age = 21 // age in years
+let isMarried = false
+// console.log(firstName, lastName, country, city, age, isMarried)
+
+// Declaring variables with number values
+const gravity = 9.81 // earth gravity  in m/s2
+const boilingPoint = 100 // water boiling point, temperature in °C
+const PI = 3.14 // geometrical constant
+// console.log(gravity, boilingPoint, PI)
+
+let name = 'Arda', myAge = 21, live = 'Türkiye'
+// console.log(name, myAge,live)
