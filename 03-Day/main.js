8854bc9e5e40c87de3407d129bac38b5ea83a73d
Arda Şalvarlılar <93258286+ardasalvarlilar@users.noreply.github.com>
2023-06-16 13:22:41 +0300

    Update main.js

--- 03-Day/main.js
+++ 03-Day/main.js
@@ -257,4 +257,4 @@ const date = now.getDate() // return date (1 - 31)
 const hours = now.getHours() // return number (0 - 23)
 const minutes = now.getMinutes() // return number (0 -59)
 
-console.log(`${date}/${month}/${year} ${hours}:${minutes}`) // 4/1/2020 0:56
+console.log(`${date}/${month}/${year} ${hours}:${minutes}`) //16/6/2023 13:18
